ee18b9290964c5447a674dd16a64576ce5bc7e7f
Aundre Lattie <aundre@gmail.com>
2024-12-29 12:53:12 -0500

    configured neovim dotfile

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,14 +1,19 @@
-vim.opt.number = true
-vim.opt.mouse = ""
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.o.number = true
+vim.o.mouse = ""
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.relativenumber = true
+
+vim.g.mapleader = " "
+
+vim.api.nvim_set_keymap( "n", "<Leader>rn", "<Cmd>set relativenumber!<CR>", { noremap = true, silent = true })
 
 local restore_cursor_augroup = vim.api.nvim_create_augroup( "restore_cursor_shape_on_exit", { clear = true })
 
-vim.api.nvim_create_autocmd({"VimLeave"},{
-    group = restore_cursor_augroup,
+vim.api.nvim_create_autocmd({"kimLeave"},{
+group = restore_cursor_augroup,
     desc = "restore the cursor shape on exit of neovim",
     command = "set guicursor=a:ver20",
 })
@@ -45,21 +50,87 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
     spec = {
-		-- add your plugins here
         {
-            'folke/tokyonight.nvim'
-        },
-		{
-			'stevearc/conform.nvim',
-			opts = {},
-		}
-	},
+          "saghen/blink.cmp",
+          -- optional: provides snippets for the snippet source
+          dependencies = 'rafamadriz/friendly-snippets',
 
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "habamax" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+          -- use a release tag to download pre-built binaries
+          version = '*',
+          -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+          -- build = 'cargo build --release',
+          -- If you use nix, you can build from source using latest nightly rust with:
+          -- build = 'nix run .#build-plugin',
+
+          ---@module 'blink.cmp'
+          ---@type blink.cmp.Config
+          opts = {
+            -- 'default' for mappings similar to built-in completion
+            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+            -- See the full "keymap" documentation for information on defining your own keymap.
+            keymap = { preset = 'default' },
+
+            appearance = {
+
+              -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+              -- Useful for when your theme doesn't support blink.cmp
+              -- Will be removed in a future release
+              use_nvim_cmp_as_default = true,
+              -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+              -- Adjusts spacing to ensure icons are aligned
+              nerd_font_variant = 'mono'
+
+            },
+
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+              default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+          },
+          opts_extend = { "sources.default" }
+        },
+        {
+            'neovim/nvim-lspconfig',
+            dependencies = { 'saghen/blink.cmp' },
+            opts = {
+                servers = {
+                    lua_ls = {}
+                }
+            },
+            config = function(_, opts)
+                local lspconfig = require('lspconfig')
+                for server, config in pairs(opts.servers) do
+                    -- passing config.capabilities to blink.cmp merges with the capabilities in your
+                    -- `opts[server].capabilities, if you've defined it
+                    config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+                    lspconfig[server].setup(config)
+                end
+            end,
+        },
+        {
+            "fcancelinha/nordern.nvim",
+            branch = "master",
+            priority = 1000,
+            opts = {
+                brighter_comments = true,
+                brighter_conditionals = true,
+                transparent = false,
+            },
+            config = function( _, opts )
+                require( "nordern" ).setup( opts )
+                vim.cmd.colorscheme( "nordern" )
+            end,
+        },
+        { 'stevearc/conform.nvim', opts = {} },
+
+        -- Configure any other settings here. See the documentation for more details.
+        -- colorscheme that will be used when installing plugins.
+        install = { },
+        -- automatically check for plugin updates
+        checker = { enabled = true },
+    }
 })
 
 require("conform").setup({
@@ -75,4 +146,6 @@ require("conform").setup({
 })
 
 vim.opt.termguicolors = true
-vim.cmd.colorscheme('tokyonight')
+vim.cmd [[highlight Normal guibg=#282828]]
+vim.cmd [[highlight MsgArea guibg=#282828]]
+vim.cmd [[highlight BufferLine guibg=#282828]]
