a438c657017310c806344a55f97f81d08d5b1950
Aundre Lattie <aundre@gmail.com>
2024-12-29 09:04:13 -0500

    update

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,5 +1,78 @@
-vim.opt.number = true;
-vim.opt.mouse = "";
-vim.opt.tabstop = 4;
-vim.opt.softtabstop = 4;
-vim.opt.shiftwidth = 4;
+vim.opt.number = true
+vim.opt.mouse = ""
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+local restore_cursor_augroup = vim.api.nvim_create_augroup( "restore_cursor_shape_on_exit", { clear = true })
+
+vim.api.nvim_create_autocmd({"VimLeave"},{
+    group = restore_cursor_augroup,
+    desc = "restore the cursor shape on exit of neovim",
+    command = "set guicursor=a:ver20",
+})
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = {
+		-- add your plugins here
+        {
+            'folke/tokyonight.nvim'
+        },
+		{
+			'stevearc/conform.nvim',
+			opts = {},
+		}
+	},
+
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- You can customize some of the format options for the filetype (:help conform.format)
+    rust = { "rustfmt", lsp_format = "fallback" },
+    -- Conform will run the first available formatter
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+  },
+})
+
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('tokyonight')
