10977233b5517ca9ad3ee578c45a8e78ad48aeed
Aundre Lattie <aundre@gmail.com>
2025-10-30 13:13:44 -0400

    added installer nix configuration

--- install/configuration.nix
+++ install/configuration.nix
@@ -1,5 +1,11 @@
-services.openssh = {
+{ config, pkgs, ... }:
+
+{
+  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix> ];
+
+  services.openssh = {
     enable = true;
     ports = [ 22 ];
-    passwordAuthentication = true;
+    passwordAuthentication  = true;
+  };
 }
