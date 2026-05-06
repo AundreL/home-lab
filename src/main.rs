f8b0a3b65a6029e5c412d294c05b10b9c7deae46
Aundre Lattie <aundre@gmail.com>
2026-05-06 14:29:14 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,44 @@ enum Commands {
         #[command(subcommand)]
         subcommand:  Option<NixIsoSubCommands>
     },
-    DevWslFlake{},
-    DevBoxFlake{},
-    Nix{},
+    NixHosts{
+        #[command(subcommand)]
+        subcommand:  Option<NixHostsSubCommands>
+    },
+    NixShells{
+        #[command(subcommand)]
+        subcommand:  Option<NixShellsSubCommands>
+    },
+    Nix{
+        #[command(subcommand)]
+        subcommand:  Option<NixSubCommands>
+    },
 }
 
 #[derive(Subcommand)]
 enum NixIsoSubCommands{
     BuildIsoProd{},
     BuildIsoDev{},
+}
+
+#[derive(Subcommand)]
+enum NixHostsSubCommands{
+    BuildDevWsl{},
+    BuildDevBox{},
+    BuildDevBoxVm{},
+}
+
+#[derive(Subcommand)]
+enum NixShellsSubCommands{
+    StartTauri{},
+}
+
+#[derive(Subcommand)]
+enum NixSubCommands{
+    CreateSecrets{},
+    InitStruct{},
+    ResyncStruct{},
+    Clean{}
 }
 
 fn main() {
@@ -42,12 +71,12 @@ mod tests{
  *build-nix-iso-verbose         X
  *build-dev-wsl-flake           X
  *build-dev-box-flake           X
- *build-dev-box-vm-flake
- *create-nix-secrets
- *start-tuari-shell
- *init-struct(nix)
- *resync-nix-struct
- *clean-nix
+ *build-dev-box-vm-flake        X
+ *start-tuari-shell             X
+ *nix-init-struct(nix)          X
+ *nix-resync-struct(nix)        X
+ *nix-create-secrets            X
+ *nix-clean                     X
  *default/help
  *
  */
@@ -84,40 +113,145 @@ mod tests{
     }
 
     #[test]
-    fn dev_wsl_flake() {
-        let args = vec![ PROGRAM_NAME, "dev-wsl-flake" ];
+    fn nix_host_dev_wsl() {
+        let args = vec![ PROGRAM_NAME, "nix-hosts", "build-dev-wsl" ];
         let cli = Cli::try_parse_from(args).unwrap();
 
         match &cli.command{
-            Some(Commands::DevWslFlake {}) => {
-               todo!("not implemented");
+            Some(Commands::NixHosts { subcommand}) => {
+                match subcommand{
+                    Some(NixHostsSubCommands::BuildDevWsl {}) => {
+                        todo!("not implemented");
+                    }
+                    _ => {}
+                }
             }
             _ => {}
         }
     }
 
     #[test]
-    fn dev_box_flake() {
-        let args = vec![ PROGRAM_NAME, "dev-box-flake" ];
+    fn nix_host_dev_box() {
+        let args = vec![ PROGRAM_NAME, "nix-hosts", "build-dev-box" ];
         let cli = Cli::try_parse_from(args).unwrap();
 
         match &cli.command{
-            Some(Commands::DevBoxFlake {}) => {
-               todo!("not implemented");
+            Some(Commands::NixHosts { subcommand }) => {
+                match subcommand {
+                    Some (NixHostsSubCommands::BuildDevBox {}) =>{
+                        todo!("not implemented");
+                    }
+                    _ => {}
+                }
             }
             _ => {}
         }
     }
 
     #[test]
-    fn nix() {
-        let args = vec![ PROGRAM_NAME, "nix" ];
+    fn nix_host_dev_box_vm() {
+        let args = vec![ PROGRAM_NAME, "nix-hosts", "build-dev-box-vm" ];
         let cli = Cli::try_parse_from(args).unwrap();
 
         match &cli.command{
-            Some(Commands::Nix {}) => {
-               todo!("not implemented");
+            Some(Commands::NixHosts { subcommand }) => {
+                match subcommand {
+                    Some (NixHostsSubCommands::BuildDevBoxVm {}) =>{
+                        todo!("not implemented");
+                    }
+                    _ => {}
+                }
             }
+            _ => {}
+        }
+    }
+
+    #[test]
+    fn nix_shell_tauri(){
+        let args = vec![ PROGRAM_NAME, "nix-shells", "start-tauri" ];
+        let cli = Cli::try_parse_from(args).unwrap();
+        
+        match &cli.command{
+            Some(Commands::NixShells { subcommand }) => {
+                match subcommand {
+                    Some (NixShellsSubCommands::StartTauri{}) => {
+                        todo!("not implemented");
+                    },
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+    }
+    
+    #[test]
+    fn nix_init_struct() {
+        let args = vec![ PROGRAM_NAME, "nix", "init-struct" ];
+        let cli = Cli::try_parse_from(args).unwrap();
+
+        match &cli.command{
+            Some(Commands::Nix { subcommand }) => {
+                match subcommand {
+                    Some(NixSubCommands::InitStruct{} ) => {
+                        todo!("not implemented");
+                    },
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+    }
+
+    #[test]
+    fn nix_resync_struct() {
+        let args = vec![ PROGRAM_NAME, "nix", "resync-struct" ];
+        let cli = Cli::try_parse_from(args).unwrap();
+
+        match &cli.command{
+            Some(Commands::Nix { subcommand }) => {
+                match subcommand {
+                    Some(NixSubCommands::ResyncStruct{} ) => {
+                        todo!("not implemented");
+                    },
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+    }
+
+    #[test]
+    fn nix_create_secrets() {
+        let args = vec![ PROGRAM_NAME, "nix", "create-secrets" ];
+        let cli = Cli::try_parse_from(args).unwrap();
+
+        match &cli.command{
+            Some(Commands::Nix { subcommand }) => {
+                match subcommand {
+                    Some(NixSubCommands::CreateSecrets{} ) => {
+                        todo!("not implemented");
+                    },
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+    }
+
+    #[test]
+    fn nix_clean() {
+        let args = vec![ PROGRAM_NAME, "nix", "clean" ];
+        let cli = Cli::try_parse_from(args).unwrap();
+
+        match &cli.command{
+            Some(Commands::Nix { subcommand }) => {
+                match subcommand {
+                    Some(NixSubCommands::Clean{} ) => {
+                        todo!("not implemented");
+                    },
+                    _ => {}
+                }
+            },
             _ => {}
         }
     }
