d8900223143e2fcdd2efe1239e0c1d70d31b4e91
Aundre Lattie <aundre@gmail.com>
2026-06-05 13:52:21 -0400

    updated kube-node to work with refactored flake

--- nix-flakes/flake.nix
+++ nix-flakes/flake.nix
@@ -1,139 +1,141 @@
 {
-    description = "home lab flake configuration";
+   description = "home lab flake configuration";
 
-    inputs = {
-        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-        nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-        home-manager.url = "github:nix-community/home-manager/release-25.11";
-        aundre-dotfiles = {
-            url = "github:AundreL/dotfiles";
-            flake = false;
-        };
+   inputs = {
+      nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+      home-manager.url = "github:nix-community/home-manager/release-25.11";
+      aundre-dotfiles = {
+         url = "github:AundreL/dotfiles";
+         flake = false;
+      };
 
-    };
+   };
 
-    outputs =
-        {
-            self,
-            nixpkgs-stable,
-            nixpkgs-unstable,
-            home-manager,
-            aundre-dotfiles,
-            ...
-        }@inputs:
-        let
-            system = builtins.currentSystem;
-            lib = nixpkgs-stable.lib;
-            pkgs-stable = nixpkgs-stable.legacyPackages.${system};
-            pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+   outputs =
+      {
+         self,
+         nixpkgs-stable,
+         nixpkgs-unstable,
+         home-manager,
+         aundre-dotfiles,
+         ...
+      }@inputs:
+      let
+         system = builtins.currentSystem;
+         lib = nixpkgs-stable.lib;
+         pkgs-stable = nixpkgs-stable.legacyPackages.${system};
+         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
 
-            secrets = builtins.fromJSON (builtins.readFile ./secrets.json);
+         secrets = builtins.fromJSON (builtins.readFile ./secrets.json);
 
-            hl-util = pkgs-stable.rustPlatform.buildRustPackage {
-                pname = "hl-util";
-                version = "0.0.3";
-                src = ./.;
+         #homelab utility tool
+         hl-util = pkgs-stable.rustPlatform.buildRustPackage {
+            pname = "hl-util";
+            version = "0.0.3";
+            src = ./.;
 
-                cargoHash = "sha256-ThS9JFVkKPKpz1/vBYmaBNcCX3NejtRwwuFmw2tI60E=";
+            cargoHash = "sha256-ThS9JFVkKPKpz1/vBYmaBNcCX3NejtRwwuFmw2tI60E=";
 
-                nativeBuildInputs = [ pkgs-stable.openssh ];
-                outputs = [
-                    "out"
-                ];
-                meta = {
-                    description = "utility tool to manage home lab";
-                    homepage = "";
-                    license = "";
-                };
+            nativeBuildInputs = [ pkgs-stable.openssh ];
+            outputs = [
+               "out"
+            ];
+            meta = {
+               description = "utility tool to manage home lab";
+               homepage = "";
+               license = "";
+            };
+         };
+
+         defaultModuleArgs = {
+            inherit pkgs-unstable; # pass unstable to modules
+            inherit pkgs-stable;
+            inherit home-manager;
+         };
+
+      in
+      {
+         nixosConfigurations.iso-installer = nixpkgs-stable.lib.nixosSystem {
+            system = system;
+            specialArgs = defaultModuleArgs // {
+               secrets = secrets;
+            };
+            modules = [
+               ./hosts/iso-installer/configuration.nix
+            ];
+         };
+
+         nixosConfigurations.dev-box = nixpkgs-stable.lib.nixosSystem {
+            system = system;
+            specialArgs = defaultModuleArgs;
+            modules = [
+               ./configuration.nix
+               ./hosts/dev-box/hardware-configuration.nix
+               ./hosts/dev-box/configuration.nix
+               ./users/aundre/configuration.nix
+            ];
+         };
+
+         #system for testing configs on vm before using on my main system
+         nixosConfigurations.dev-box-vm = nixpkgs-stable.lib.nixosSystem {
+            system = system;
+            specialArgs = defaultModuleArgs;
+            modules = [
+               ./configuration.nix
+               ./hosts/kube-node/hardware-configuration.nix
+               ./hosts/dev-box/configuration.nix
+               ./users/aundre/configuration.nix
+            ];
+         };
+
+         nixosConfigurations.dev-wsl = nixpkgs-stable.lib.nixosSystem {
+            system = system;
+            specialArgs = defaultModuleArgs // {
+               inherit aundre-dotfiles;
+               secrets = secrets;
+               hl-util = hl-util;
             };
 
-            defaultModuleArgs = {
-                inherit pkgs-unstable; # pass unstable to modules
-                inherit pkgs-stable;
-                inherit home-manager;
+            modules = [
+               ./configuration.nix
+               ./hosts/dev-wsl/configuration.nix
+               ./users/aundre/configuration.nix
+            ];
+
+         };
+
+         nixosConfigurations.kube-node = nixpkgs-stable.lib.nixosSystem {
+            system = system;
+            specialArgs = defaultModuleArgs // {
+               secrets = secrets;
             };
+            modules = [
+               ./hosts/kube-node/hardware-configuration.nix
+               ./hosts/kube-node/configuration.nix
+            ];
+         };
 
-        in
-        {
-            nixosConfigurations.iso-installer = nixpkgs-stable.lib.nixosSystem {
-                system = system;
-                specialArgs = defaultModuleArgs // {
-                    secrets = secrets;
-                };
-                modules = [
-                    ./hosts/iso-installer/configuration.nix
-                ];
-            };
+         devShells.x86_64-linux.tauri = pkgs-stable.mkShell {
+            nativeBuildInputs = with pkgs-stable; [
+               pkg-config
+               wrapGAppsHook4
+               cargo
+               cargo-tauri
+               rustc
+               bun
+            ];
 
-            nixosConfigurations.dev-box = nixpkgs-stable.lib.nixosSystem {
-                system = system;
-                specialArgs = defaultModuleArgs;
-                modules = [
-                    ./configuration.nix
-                    ./hosts/dev-box/hardware-configuration.nix
-                    ./hosts/dev-box/configuration.nix
-                    ./users/aundre/configuration.nix
-                ];
-            };
+            buildInputs = with pkgs-stable; [
+               nodePackages."@angular/cli"
+               librsvg
+               webkitgtk_4_1
+            ];
 
-            #system for testing configs on vm before using on my main system
-            nixosConfigurations.dev-box-vm = nixpkgs-stable.lib.nixosSystem {
-                system = system;
-                specialArgs = defaultModuleArgs;
-                modules = [
-                    ./configuration.nix
-                    ./hosts/kube-node/hardware-configuration.nix
-                    ./hosts/dev-box/configuration.nix
-                    ./users/aundre/configuration.nix
-                ];
-            };
-
-            nixosConfigurations.dev-wsl = nixpkgs-stable.lib.nixosSystem {
-                system = system;
-                specialArgs = defaultModuleArgs // {
-                    inherit aundre-dotfiles;
-                    secrets = secrets;
-                    hl-util = hl-util;
-                };
-
-                modules = [
-                    ./configuration.nix
-                    ./hosts/dev-wsl/configuration.nix
-                    ./users/aundre/configuration.nix
-                ];
-
-            };
-
-            nixosConfigurations.kube-node = nixpkgs-stable.lib.nixosSystem {
-                system = system;
-                specialArgs = defaultModuleArgs;
-                modules = [
-                    ./configuration.nix
-                    ./hosts/kube-node/hardware-configuration.nix
-                    ./hosts/kube-node/configuration.nix
-                ];
-            };
-
-            devShells.x86_64-linux.tauri = pkgs-stable.mkShell {
-                nativeBuildInputs = with pkgs-stable; [
-                    pkg-config
-                    wrapGAppsHook4
-                    cargo
-                    cargo-tauri
-                    rustc
-                    bun
-                ];
-
-                buildInputs = with pkgs-stable; [
-                    nodePackages."@angular/cli"
-                    librsvg
-                    webkitgtk_4_1
-                ];
-
-                shellHook = ''
-                    echo "starting tauri shell"
-                    export XDG_DATA_DIRS="$GSETTINGS_SCHEMAS_PATH" # Needed on Wayland to report the correct display scale
-                '';
-            };
-        };
+            shellHook = ''
+               echo "starting tauri shell"
+               export XDG_DATA_DIRS="$GSETTINGS_SCHEMAS_PATH" # Needed on Wayland to report the correct display scale
+            '';
+         };
+      };
 }
--- nix-flakes/hosts/iso-installer/configuration.nix
+++ nix-flakes/hosts/iso-installer/configuration.nix
@@ -1,122 +1,127 @@
 {
-    config,
-    lib,
-    pkgs-stable,
-    pkgs-unstable,
-    home-manager,
-    secrets,
-    ...
+   config,
+   lib,
+   pkgs-stable,
+   pkgs-unstable,
+   home-manager,
+   secrets,
+   ...
 }:
 let
-    iso-utils = derivation {
-        name = "iso-utils";
-        system = builtins.currentSystem;
-        builder = "${pkgs-stable.bash}/bin/bash";
-        flake_dir = ../..;
-        scripts_dir = ../../scripts;
-        args = [
-            "-c"
-            ''
-                export PATH=$PATH:${pkgs-stable.coreutils}/bin
+   iso-utils = derivation {
+      name = "iso-utils";
+      system = builtins.currentSystem;
+      builder = "${pkgs-stable.bash}/bin/bash";
+      flake_dir = ../..;
+      scripts_dir = ../../scripts;
+      args = [
+         "-c"
+         ''
+            export PATH=$PATH:${pkgs-stable.coreutils}/bin
 
-                mkdir -p $out/etc/iso-utils
-                mkdir -p $out/bin
-                echo $out >&2
-                echo $scripts_dir >&2
-                 
-                echo "flake_dir: $flake_dir"
-                echo "scripts_dir: $scripts_dir"
-                echo "derivation location: $out"
+            mkdir -p $out/etc/iso-utils
+            mkdir -p $out/bin
+            echo $out >&2
+            echo $scripts_dir >&2
+             
+            echo "flake_dir: $flake_dir"
+            echo "scripts_dir: $scripts_dir"
+            echo "derivation location: $out"
 
-                for file in $scripts_dir/*.sh; do
-                    echo "loop tracker $file"
-                    basefile=$(basename -- ''${file%.sh})
-                    echo "base $basefile"
-                    cp $file $out/bin/$basefile
-                    chmod 777 $out/bin/$basefile
-                done
+            for file in $scripts_dir/*.sh; do
+                echo "loop tracker $file"
+                basefile=$(basename -- ''${file%.sh})
+                echo "base $basefile"
+                cp $file $out/bin/$basefile
+                chmod 777 $out/bin/$basefile
+            done
 
-                cp -r $flake_dir/. $out/etc/iso-utils/flakes
-            ''
-        ];
-    };
+            cp -r $flake_dir/. $out/etc/iso-utils/flakes
+         ''
+      ];
+   };
+
+   stable-packages = with pkgs-stable; [
+      git
+      gnumake
+      python3
+      tree
+      wget
+      curl
+      neovim
+      iso-utils
+      htop
+   ];
+
+   custom-packages = [
+   ];
 in
 {
-    imports = [
-        (import "${home-manager}/nixos")
-        <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-        <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-    ];
+   imports = [
+      (import "${home-manager}/nixos")
+      <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+      <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+   ];
 
-    i18n.supportedLocales = [
-        "en_US.UTF-8/UTF-8"
-    ];
+   i18n.supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+   ];
 
-    i18n.defaultLocale = "en_US.UTF-8";
+   i18n.defaultLocale = "en_US.UTF-8";
 
-    nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-    ];
-    nixpkgs.config.allowUnfree = true;
+   nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
+   ];
+   nixpkgs.config.allowUnfree = true;
 
-    security.sudo.wheelNeedsPassword = false;
+   security.sudo.wheelNeedsPassword = false;
 
-    users.users.nixos = {
-        isNormalUser = true;
-        description = "nixos";
-        extraGroups = [ "wheel" ];
-        shell = pkgs-stable.fish;
+   users.users.nixos = {
+      isNormalUser = true;
+      description = "nixos";
+      extraGroups = [ "wheel" ];
+      shell = pkgs-stable.fish;
 
-        openssh.authorizedKeys.keys = [
-            secrets.dev_box_nixos
-            secrets.cluster_node_nixos
-        ];
-    };
+      openssh.authorizedKeys.keys = [
+         secrets.dev_box_nixos
+         secrets.cluster_node_nixos
+      ];
+   };
 
-    home-manager.users.nixos = {
-        home.stateVersion = "25.11";
-    };
+   home-manager.users.nixos = {
+      home.stateVersion = "25.11";
+   };
 
-    environment.etc = {
-        "iso-utils" = {
-            source = "${iso-utils}/etc/iso-utils/";
-        };
-    };
+   environment.etc = {
+      "iso-utils" = {
+         source = "${iso-utils}/etc/iso-utils/";
+      };
+   };
 
-    environment.systemPackages = with pkgs-stable; [
-        git
-        gnumake
-        python3
-        tree
-        wget
-        curl
-        neovim
-        iso-utils
-        htop
-    ];
+   environment.systemPackages = stable-packages ++ custom-packages;
 
-    programs.fish = {
-        enable = true;
-        interactiveShellInit = ''
-            fish_vi_key_bindings
-        '';
-    };
+   programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+         fish_vi_key_bindings
+      '';
+   };
 
-    services.openssh = {
-        enable = true;
-        ports = [ 22 ];
-        settings = {
-            PermitRootLogin = "no";
-            PasswordAuthentication = false;
-        };
-    };
+   services.openssh = {
+      enable = true;
+      ports = [ 22 ];
+      settings = {
+         PermitRootLogin = "no";
+         PasswordAuthentication = false;
+      };
+   };
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "25.11"; # Did you read the comment?
+   # This value determines the NixOS release from which the default
+   # settings for stateful data, like file locations and database versions
+   # on your system were taken. It‘s perfectly fine and recommended to leave
+   # this value at the release version of the first install of this system.
+   # Before changing this value read the documentation for this option
+   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+   system.stateVersion = "25.11"; # Did you read the comment?
 }
