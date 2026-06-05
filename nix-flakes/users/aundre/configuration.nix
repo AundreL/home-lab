379239cbfa505ab389dbcdea862269808c962f8d
Aundre Lattie <aundre@gmail.com>
2026-06-05 13:51:50 -0400

    changed secrets variable to be loaded from import instead of relative path

--- nix-flakes/users/aundre/configuration.nix
+++ nix-flakes/users/aundre/configuration.nix
@@ -3,81 +3,81 @@
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 {
-    config,
-    lib,
-    pkgs-stable,
-    pkgs-unstable,
-    home-manager,
-    aundre-dotfiles,
-    secrets,
-    ...
+   config,
+   lib,
+   pkgs-stable,
+   pkgs-unstable,
+   home-manager,
+   aundre-dotfiles,
+   secrets,
+   ...
 }:
 {
-    imports = [
-        (import "${home-manager}/nixos")
-    ];
+   imports = [
+      (import "${home-manager}/nixos")
+   ];
 
-    programs.fish.enable = true;
-    programs.starship.enable = true;
+   programs.fish.enable = true;
+   programs.starship.enable = true;
 
-    users.users.aundre = builtins.trace "dotfiles location: ${aundre-dotfiles}" {
-        isNormalUser = true;
-        home = "/home/aundre";
-        extraGroups = [
-            "wheel"
-            "networkmanager"
-        ];
-        shell = pkgs-stable.fish;
+   users.users.aundre = builtins.trace "dotfiles location: ${aundre-dotfiles}" {
+      isNormalUser = true;
+      home = "/home/aundre";
+      extraGroups = [
+         "wheel"
+         "networkmanager"
+      ];
+      shell = pkgs-stable.fish;
 
-        openssh.authorizedKeys.keys = [
-            secrets.dev_box_aundre
-        ];
-    };
+      openssh.authorizedKeys.keys = [
+         secrets.dev_box_aundre
+      ];
+   };
 
-    home-manager.users.aundre = {
-        programs.neovim.defaultEditor = true;
+   home-manager.users.aundre = {
+      programs.neovim.defaultEditor = true;
 
-        programs.git = {
-            enable = true;
-            settings = {
-                user.name = "Aundre Lattie";
-                user.email = "aundre@gmail.com";
-                core.editor = "nvim";
-            };
-        };
+      programs.git = {
+         enable = true;
+         settings = {
+            user.name = "Aundre Lattie";
+            user.email = "aundre@gmail.com";
+            core.editor = "nvim";
+         };
+      };
 
-        home.file = {
-            ".config/nvim" = {
-                source = aundre-dotfiles + /nvim;
-                recursive = true;
-            };
-        };
-
-        home.file = {
-            ".ssh/config" = {
-                source = aundre-dotfiles + /ssh-config;
-            };
-        };
-
-        home.file.".config/fish" = {
-            source = aundre-dotfiles + /fish;
+      home.file = {
+         ".config/nvim" = {
+            source = aundre-dotfiles + /nvim;
             recursive = true;
-        };
+         };
+      };
 
-        home.file = {
-            ".config/starship.toml" = {
-                source = aundre-dotfiles + /starship.toml;
-            };
-        };
+      home.file = {
+         ".ssh/config" = {
+            source = aundre-dotfiles + /ssh-config;
+         };
+      };
 
-        home.file = {
-            ".tmux.conf" = {
-                source = aundre-dotfiles + /tmux/tmux.conf;
-            };
-        };
+      home.file.".config/fish" = {
+         source = aundre-dotfiles + /fish;
+         recursive = true;
+      };
 
-        programs.home-manager.enable = true;
+      home.file = {
+         ".config/starship.toml" = {
+            source = aundre-dotfiles + /starship.toml;
+         };
+      };
 
-        home.stateVersion = "25.11";
-    };
+      home.file = {
+         ".tmux.conf" = {
+            source = aundre-dotfiles + /tmux/tmux.conf;
+         };
+      };
+
+      programs.home-manager.enable = true;
+
+      home.stateVersion = "25.11";
+   };
 }
